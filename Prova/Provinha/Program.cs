4489590eaeb07a741e75770a8c39089305244106
Kronossss <iafsalmeida.cic@uesc.br>
2023-11-30 16:19:08 -0300

    Comit3

--- Prova/Provinha/Program.cs
+++ Prova/Provinha/Program.cs
@@ -8,6 +8,7 @@ class Program {
         List<Cliente> clientes= new List<Cliente>();
 
         try {
+
             advogados.Add(new Advogado("Adv1", new DateTime (1980, 1, 1),"08419437530","CNA1"));
             advogados.Add(new Advogado("Adv2", new DateTime (1998, 7, 7),"12345678912","CNA2"));
             
@@ -16,8 +17,37 @@ class Program {
             clientes.Add(new Cliente("Cli2",new DateTime (1998, 5, 5),"08419437532","Casado","Prof2"));
         }
         catch (ArgumentException ex){
-            Console.WriteLine($"Erro ao adcionar pesosa :  {ex.Message}");
+            Console.WriteLine($"Erro ao adcionar pessoa ou advogado  :  {ex.Message}");
         }
 
     }
-}   
+}
+
+class Pessoa
+{
+    public string Nome {get; set;}
+    public DateTime DataNascimento {get; set;}
+    public string CPF {get; set;}
+
+    public Pessoa(string nome, DateTime dataNascimento, string cpf){
+        Nome = nome;
+        DataNascimento = dataNascimento;
+        CPF = cpf;
+
+    }
+    private void SetCpf(string cpf){
+        if (cpf.Length != 11 || !cpf.All(char.IsDigit)){
+            throw new ArgumentException("CPF INVÁLIDO");
+        }
+        CPF = cpf;              
+    }
+}
+
+class Advogado : Pessoa 
+{
+    public string CNA {get; set;}
+
+    public Advogado (string nome, DateTime dataNascimento, string cpf, string cna) : base(nome, dataNascimento,cpf){
+        CNA = cna;
+    }
+}
